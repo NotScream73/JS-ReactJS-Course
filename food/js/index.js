d08535487048cefa2565c2375d175ba81060553e
Nikita Sergeev <notscreamplay73@gmail.com>
2023-08-01 21:27:26 +0400

    Axios

--- food/js/index.js
+++ food/js/index.js
@@ -193,12 +193,12 @@ window.addEventListener('DOMContentLoaded', () => {
     return await res.json();
   }
 
-  getResource('http://localhost:3000/menu')
-    .then(data => {
-      data.forEach(({ img, altimg, title, descr, price }) => {
-        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-      })
-    });
+  // getResource('http://localhost:3000/menu')
+  //   .then(data => {
+  //     data.forEach(({ img, altimg, title, descr, price }) => {
+  //       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //     })
+  //   });
   // 2 вариант
 
   // getResource('http://localhost:3000/menu')
@@ -223,6 +223,13 @@ window.addEventListener('DOMContentLoaded', () => {
   //     document.querySelector('.menu .container').append(element);
   //   })
   // }
+
+  axios.get('http://localhost:3000/menu')
+    .then(data => {
+      data.data.forEach(({ img, altimg, title, descr, price }) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      });
+    });
 
   // Forms
 
